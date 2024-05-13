e833a134ea29d3fda46ce4177b4eeb34343041b8
mastaan66 <mastaanshaik37@gmail.com>
2024-05-13 15:14:24 +0530

    strings in js

---
+++ strings.js
@@ -0,0 +1,14 @@
+let userName = "mastan"
+let age = 19
+
+// console.log(userName + age ); // outdated way to print some concated message
+
+// string interpolation ==> ${} --> placeholder
+
+console.log(`hey i'm ${userName} and my age is ${age}`)
+
+console.log(userName.toUpperCase()); // it didn't change the string because it's a primitive type
+
+console.log(`original ${userName}`)
+
+// 2:16:00 --> time
