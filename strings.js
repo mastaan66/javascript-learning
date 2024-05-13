c00924256a416a5ead446a8fc99985b0714fbb1f
mastaan66 <mastaanshaik37@gmail.com>
2024-05-13 18:21:03 +0530

    string methods

--- strings.js
+++ strings.js
@@ -5,10 +5,27 @@ let age = 19
 
 // string interpolation ==> ${} --> placeholder
 
-console.log(`hey i'm ${userName} and my age is ${age}`)
+// console.log(`hey i'm ${userName} and my age is ${age}`)
 
-console.log(userName.toUpperCase()); // it didn't change the string because it's a primitive type
+// console.log(userName.toUpperCase()); // it didn't change the string because it's a primitive type
 
-console.log(`original ${userName}`)
+// console.log(`original ${userName}`)
 
-// 2:16:00 --> time
+// console.log(`the length of ${userName} is ${userName.length}`);
+
+// string methods
+// console.log(userName.toUpperCase());
+// console.log(userName.charAt(3));
+// console.log(userName.slice(-6,3));
+// console.log(userName.indexOf('s'));
+const substr = userName.substring(0,2);
+// console.log("   hey     ");
+// console.log("   hey\nhey".trim());
+
+const url = "https://google.com/mastaan%20shaik";
+// console.log(url.replace('%20','-'));
+
+// convert string as array based on something
+const str = "mastan shaik is here to tell you something"
+const arr = str.split(' ');
+console.log(arr);
